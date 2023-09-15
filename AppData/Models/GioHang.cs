ac787db41039404901b837e3d22cef20f5396946
MI\thatt <thatthienvan20@gmail.com>
2023-09-15 22:05:17 +0700

    xong model

---
+++ AppData/Models/GioHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class GioHang
+    {
+        public Guid IDUser { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int TrangThai { get; set; }
+        //public virtual IEnumerable<> GioHangCt { get; set; }
+        //public virtual User User { get; set; }
+    }
+}
---
+++ AppData/Models/KichCo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class KichCo
+    {
+        public Guid IDKichCo { get; set; }
+        public string Ma { get; set; }
+        public int SoZise { get; set; }
+        public int TrangThai { get; set; }
+    }
+}
