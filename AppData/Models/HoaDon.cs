e35b0a21b7afcf1cade688e306e048554290cd20
DESKTOP-8RSAFN0\anhtr <anhtrinhtran3@gmail.com>
2023-09-15 21:54:35 +0700

    add model cardetailst,bill,billdetails,loaigiay

---
+++ AppData/Models/HoaDon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class HoaDon
+    {
+        public Guid IdHoaDon { get; set; }
+        public Guid? IdUser { get; set; }
+        public Guid? IdVoucher { get; set; }
+        public Guid? IdThongTinGH { get; set; }
+        public string? MaHoaDon { get; set; }
+        public DateTime? NgayTao { get; set; }
+        public DateTime? NgayThanhToan { get; set; }
+        public DateTime? NgayShip { get; set; }
+        public DateTime? NgayNhan { get; set; }
+        public double? TienShip { get; set; }
+        public double? TienGiam { get; set; }
+        public double? TongTien { get; set; }
+        public string? MoTa { get; set; }
+        public int? TrangThai { get; set; }
+        public int? TrangThaiThanhToan { get; set; }
+        public virtual List<HoaDonChiTiet> HoaDonChiTiet { get; set; }
+        //public virtual PhuongThucGiaoHang PhuongThucGiaoHang { get; set; }
+        //public virtual Voucher Voucher { get; set; }
+        //public virtual List<PhuongThucThanhToanChiTiet> PhuongThucThanhToanChiTiet { get; set; }
+    }
+}
---
+++ AppData/Models/HoaDonChiTiet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class HoaDonChiTiet
+    {
+        public Guid IdHoaDonChiTiet { get; set; }
+        public Guid? IdHoaDon { get; set; }
+        public Guid? IdSanPhamChiTiet { get; set; }
+        public int? SoLuong { get; set; }
+        public double? GiaGoc { get; set; }
+        public double? GiaBan { get; set; }
+        public int? TrangThai { get; set; }
+        public virtual HoaDon HoaDon { get; set; }
+        //public virtual ProductDetail ProductDetail { get; set; }
+    }
+}
