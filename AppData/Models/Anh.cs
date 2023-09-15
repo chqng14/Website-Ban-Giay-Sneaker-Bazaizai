03709fd77b6fffd066e06dbf2d09ff3b8560dae0
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-15 21:55:57 +0700

    add models: anh, mausac, sanphamChitiet,xuatxu

---
+++ AppData/Models/Anh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class Anh
+    {
+        [Key]
+        public Guid IDAnh { get; set; }
+        public string Url { get; set; }
+        public int TrangThai { get; set; }
+
+        public Guid IdSanPhamChiTiet { get; set; }
+    }
+}
---
+++ AppData/Models/MauSac.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class MauSac
+    {
+        [Key]
+        public Guid IDMauSac { get; set; }
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public int TrangThai { get; set; }
+    }
+}
