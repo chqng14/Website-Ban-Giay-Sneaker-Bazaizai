db6a25cabe7fcfdd0a71b58c03b6b1b13fbf0711
chqng14 <phuongchang1402@gmail.com>
2023-09-15 21:52:28 +0700

    Xong model CHatLieu, KieuDegiay, PHuongThucThanhToan, Phuong thanh toan chi tiet, Và sản phẩm

---
+++ AppData/Models/PhuongThucThanhToanChiTiet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class PhuongThucThanhToanChiTiet
+    {
+        public Guid IDPhuongThucThanhToanChiTiet { get; set; }
+        public Guid IdHoaDon { get; set; }
+        public Guid IdThanhToan { get; set; }
+
+        public double SoTien { get; set; }
+        public virtual PhuongThucThanhToan PhuongThucThanhToan { get; set; }
+        //public virtual HoaDon HoaDons { get; set; }
+    }
+}
---
+++ AppData/Models/SanPham.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Models
+{
+    public class SanPham
+    {
+        public Guid IdSanPham { get; set; }
+        public string MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public int Trangthai { get; set; }
+        //public virtual List<SanPhamChiTiet> SanPhamChiTiets { get; set; }
+    }
+}
