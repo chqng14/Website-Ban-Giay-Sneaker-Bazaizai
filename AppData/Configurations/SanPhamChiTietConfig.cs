d50e55a5cd566c4cdddb8b25d1a495fb17e88e8b
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-15 23:33:54 +0700

    add config: anh, mau sac, xuat xu, sanphamchitiet

---
+++ AppData/Configurations/SanPhamChiTietConfig.cs
@@ -0,0 +1,58 @@
+﻿using App_Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Configurations
+{
+    public class SanPhamChiTietConfig : IEntityTypeConfiguration<SanPhamChiTiet>
+    {
+        public void Configure(EntityTypeBuilder<SanPhamChiTiet> builder)
+        {
+            builder.HasKey(x => x.IDChiTietSp);
+
+            builder.HasOne(x => x.ThuongHieu).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdThuongHieu);
+
+
+            builder.HasOne(x => x.SanPham).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdSanPham);
+
+
+            builder.HasOne(x => x.KieuDeGiay).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdKieuDeGiay);
+
+
+            builder.HasOne(x => x.XuatXu).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdXuatXu);
+
+
+            builder.HasOne(x => x.ChatLieu).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdChatLieu);
+
+
+            builder.HasOne(x => x.MauSac).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdMauSac);
+
+
+            builder.HasOne(x => x.KichCo).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdKichCo);
+
+
+            builder.HasOne(x => x.LoaiGiay).WithMany(y => y.SanPhamChiTiets).
+            HasForeignKey(c => c.IdLoaiGiay);
+
+
+            builder.Property(x => x.Ma).HasColumnType("varchar(50)");
+            builder.Property(x => x.Day).HasColumnType("nvarchar(250)");
+            builder.Property(x => x.MoTa).HasColumnType("Nvarchar(max)");
+
+
+           
+        }
+    }
+}
---
+++ AppData/Configurations/XuatXuConfig.cs
@@ -0,0 +1,21 @@
+﻿using App_Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Data.Configurations
+{
+    public class XuatXuConfig : IEntityTypeConfiguration<XuatXu>
+    {
+        public void Configure(EntityTypeBuilder<XuatXu> builder)
+        {
+            builder.HasKey(x => x.IDXuatXu);
+            builder.Property(x => x.Ma).HasColumnType("varchar(50)");
+            builder.Property(x => x.Ten).HasColumnType("nvarchar(250)");
+        }
+    }
+}
