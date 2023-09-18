1b17110693e085ce9fddfecd925f550b82677be6
MI\thatt <thatthienvan20@gmail.com>
2023-09-19 00:04:50 +0700

    Xong phần Api của bản thân

---
+++ App_Api/Controllers/KhuyenMaiChiTietController.cs
@@ -0,0 +1,72 @@
+﻿using App_Data.DbContextt;
+using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OpenXmlPowerTools;
+
+namespace App_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhuyenMaiChiTietController : ControllerBase
+    {
+        private readonly IAllRepo<KhuyenMaiChiTiet> repos;
+        BazaizaiContext context = new BazaizaiContext();
+        DbSet<KhuyenMaiChiTiet> khuyenMaiChiTiets;
+        public KhuyenMaiChiTietController()
+        {
+            khuyenMaiChiTiets = context.khuyenMaiChiTiets;
+            AllRepo<KhuyenMaiChiTiet> all = new AllRepo<KhuyenMaiChiTiet>(context, khuyenMaiChiTiets);
+            repos = all;
+        }
+        [HttpGet]
+        public IEnumerable<KhuyenMaiChiTiet> GetAllKhuyenMai()
+        {
+            return repos.GetAll();
+        }
+
+        [HttpPost]
+
+        public bool CreateKhuyenMaiChiTiet(string mota, int trangThai, Guid IDKm, Guid IDSpCt)
+        {
+            string MaTS;
+            if (repos.GetAll().Count() == null)
+            {
+                MaTS = "KMCT1";
+            }
+            else
+            {
+                MaTS = "KMCT" + (repos.GetAll().Count() + 1);
+            }
+            KhuyenMaiChiTiet b = new KhuyenMaiChiTiet();
+            b.IDKhuyenMaiChiTiet = Guid.NewGuid();
+            b.IDKhuyenMai = IDKm;
+            b.IDSanPhamChiTiet = IDSpCt;
+            b.MoTa=mota;
+            b.TrangThai=trangThai;
+            return repos.AddItem(b);
+        }
+
+
+        [HttpPut("{id}")]
+        public bool EditKhuyenMaiChiTiet(Guid id, string mota, int trangThai, Guid IDKm, Guid IDSpCt)
+        {
+            var b = repos.GetAll().First(p => p.IDKhuyenMaiChiTiet == a.IDKhuyenMaiChiTiet);
+            b.IDKhuyenMai = IDKm;
+            b.IDSanPhamChiTiet = IDSpCt;
+            b.MoTa = mota;
+            b.TrangThai = trangThai;
+            return repos.EditItem(b);
+        }
+
+        [HttpDelete("{id}")]
+        public bool DeleteKhuyenMaiChiTiet(Guid id)
+        {
+            var KhuyenMaiChiTiet = repos.GetAll().First(p => p.IDKhuyenMaiChiTiet == id);
+            return repos.RemoveItem(KhuyenMaiChiTiet);
+        }
+    }
+}
---
+++ App_Api/Controllers/ThuongHieuController.cs
@@ -0,0 +1,77 @@
+﻿using App_Data.DbContextt;
+using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThuongHieuController : ControllerBase
+    {
+        private readonly IAllRepo<ThuongHieu> repos;
+        BazaizaiContext context = new BazaizaiContext();
+        DbSet<ThuongHieu> thuongHieus;
+        public ThuongHieuController()
+        {
+            thuongHieus = context.thuongHieus;
+            AllRepo<ThuongHieu> all = new AllRepo<ThuongHieu>(context, thuongHieus);
+            repos = all;
+        }
+        [HttpGet]
+        public IEnumerable<ThuongHieu> GetAllKhuyenMai()
+        {
+            return repos.GetAll();
+        }
+
+        [HttpPost]
+
+        //public bool CreateThuongHieu(ThuongHieu a)
+        //{
+        //    ThuongHieu b = new ThuongHieu();
+        //    b.IDThuongHieu = Guid.NewGuid();
+        //    b.MaThuongHieu = a.MaThuongHieu;
+        //    b.TenThuongHieu = a.TenThuongHieu;
+        //    b.TrangThai = a.TrangThai;
+        //    return repos.AddItem(b);
+        //}
+        public bool CreateThuongHieu( int trangThai, string Ten)
+        {
+            string MaTS;
+            if (repos.GetAll().Count() == null)
+            {
+                MaTS = "TH1";
+            }
+            else
+            {
+                MaTS = "TH" + (repos.GetAll().Count() + 1);
+            }
+            ThuongHieu b = new ThuongHieu();
+            b.IDThuongHieu = Guid.NewGuid();
+            b.MaThuongHieu = MaTS;
+            b.TenThuongHieu = Ten;
+            b.TrangThai = trangThai;
+            return repos.AddItem(b);
+        }
+
+        [HttpPut("{id}")]
+        public bool EditThuongHieu(Guid id,string Ma, int trangThai, string Ten)
+        {
+            var b = repos.GetAll().First(p => p.IDThuongHieu == id);
+            b.MaThuongHieu = Ma;
+            b.TenThuongHieu = Ten;
+            b.TrangThai = trangThai;
+            return repos.EditItem(b);
+        }
+
+        [HttpDelete("{id}")]
+        public bool DeleteThuongHieu(Guid id)
+        {
+            var ThuongHieu = repos.GetAll().First(p => p.IDThuongHieu == id);
+            return repos.RemoveItem(ThuongHieu);
+        }
+    }
+}
