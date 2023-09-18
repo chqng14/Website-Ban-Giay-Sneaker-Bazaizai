028f6126ff58d1ffc3f2224d39ab7175f3d93f36
DESKTOP-8RSAFN0\anhtr <anhtrinhtran3@gmail.com>
2023-09-18 23:17:28 +0700

    them controller hoadon,loaigiay

---
+++ App_Api/Controllers/HoaDonController.cs
@@ -0,0 +1,101 @@
+﻿using App_Data.DbContextt;
+using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.Repositories;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace App_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HoaDonController : ControllerBase
+    {
+        private readonly IAllRepo<HoaDon> allRepo;
+        BazaizaiContext DbContextModel = new BazaizaiContext();
+        DbSet<HoaDon> hoaDons;
+        public HoaDonController()
+        {
+            hoaDons = DbContextModel.HoaDons;
+            allRepo = new AllRepo<HoaDon>(DbContextModel, hoaDons);
+        }
+        // GET: api/<HoaDonController>
+        [HttpGet]
+        public IEnumerable<HoaDon> Get()
+        {
+            return allRepo.GetAll();
+        }
+
+        // GET api/<HoaDonController>/5
+        [HttpGet("GetHoaDonById")]
+        public HoaDon GetHoaDonById(Guid id)
+        {
+            return allRepo.GetAll().FirstOrDefault(c => c.IdHoaDon == id);
+        }
+
+        // POST api/<HoaDonController>
+        [HttpPost("Create")]
+        public bool Post(Guid IdVoucher, Guid IdKhachHang, Guid IdThongTinGH, DateTime NgayTao, DateTime NgayThanhToan, DateTime NgayShip, DateTime NgayNhan, double TienShip, double TienGiam, double TongTien, string MoTa, int TrangThai, int TrangThaiThanhToan)
+        {
+            string ma;
+            if (allRepo.GetAll().Count() == null)
+            {
+                ma = "HD1";
+            }
+            else
+            {
+                ma = "HD" + (allRepo.GetAll().Count() + 1);
+            }
+            HoaDon hd = new HoaDon()
+            {
+                IdHoaDon = Guid.NewGuid(),
+                IdVoucher = IdVoucher,
+                IdThongTinGH = IdThongTinGH,
+                MaHoaDon = ma,
+                NgayTao = NgayTao,
+                NgayThanhToan = NgayThanhToan,
+                NgayShip = NgayShip,
+                NgayNhan = NgayNhan,
+                TienShip = TienShip,
+                TienGiam = TienGiam,
+                TongTien = TongTien,
+                MoTa = MoTa,
+                TrangThai = TrangThai,
+                TrangThaiThanhToan = TrangThaiThanhToan,
+            };
+            return allRepo.AddItem(hd);
+        }
+
+        // PUT api/<HoaDonController>/5
+        [HttpPut("Edit")]
+        public bool Put(Guid idHoaDon, Guid IdVoucher, Guid IdThongTinGH, string MaHoaDon, DateTime NgayTao, DateTime NgayThanhToan, DateTime NgayShip, DateTime NgayNhan, double TienShip, double TienGiam, double TongTien, string MoTa, int TrangThai, int TrangThaiThanhToan)
+        {
+            var hd = allRepo.GetAll().First(p => p.IdHoaDon == idHoaDon);
+            hd.IdVoucher = IdVoucher;
+            hd.IdThongTinGH = IdThongTinGH;
+            hd.MaHoaDon = MaHoaDon;
+            hd.NgayTao = NgayTao;
+            hd.NgayThanhToan = NgayThanhToan;
+            hd.NgayShip = NgayShip;
+            hd.NgayNhan = NgayNhan;
+            hd.TienShip = TienShip;
+            hd.TienGiam = TienGiam;
+            hd.TongTien = TongTien;
+            hd.MoTa = MoTa;
+            hd.TrangThai = TrangThai;
+            hd.TrangThaiThanhToan = TrangThaiThanhToan;
+            return allRepo.EditItem(hd);
+        }
+
+        // DELETE api/<HoaDonController>/5
+        [HttpDelete("Delete")]
+        public bool Delete(Guid idHoaDon)
+        {
+            var hd = allRepo.GetAll().First(p => p.IdHoaDon == idHoaDon);
+            return allRepo.RemoveItem(hd);
+        }
+    }
+}
---
+++ App_Api/Controllers/LoaiGiayController.cs
@@ -0,0 +1,80 @@
+﻿using App_Data.DbContextt;
+using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace App_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaiGiayController : ControllerBase
+    {
+        private readonly IAllRepo<LoaiGiay> allRepo;
+        BazaizaiContext DbContextModel = new BazaizaiContext();
+        DbSet<LoaiGiay> loaiGiays;
+        public LoaiGiayController()
+        {
+            loaiGiays = DbContextModel.LoaiGiays;
+            allRepo = new AllRepo<LoaiGiay>(DbContextModel, loaiGiays);
+        }
+        // GET: api/<LoaiGiayController>
+        [HttpGet]
+        public IEnumerable<LoaiGiay> Get()
+        {
+            return allRepo.GetAll();
+        }
+
+        // GET api/<LoaiGiayController>/5
+        [HttpGet("GetLoaiGiayById")]
+        public LoaiGiay GetLoaiGiayById(Guid id)
+        {
+            return allRepo.GetAll().FirstOrDefault(c => c.IdLoaiGiay == id);
+        }
+
+        // POST api/<LoaiGiayController>
+        [HttpPost("Create")]
+        public bool Post(string TenLoaiGiay, int TrangThai)
+        {
+            string ma;
+            if (allRepo.GetAll().Count() == null)
+            {
+                ma = "HD1";
+            }
+            else
+            {
+                ma = "HD" + (allRepo.GetAll().Count() + 1);
+            }
+            LoaiGiay lg = new LoaiGiay()
+            {
+                IdLoaiGiay = Guid.NewGuid(),
+                MaLoaiGiay = ma,
+                TenLoaiGiay = TenLoaiGiay,
+                TrangThai = TrangThai,
+            };
+            return allRepo.AddItem(lg);
+        }
+
+        // PUT api/<LoaiGiayController>/5
+        [HttpPut("Edit")]
+        public bool Put(Guid idLoaiGiay, string TenLoaiGiay, string MaLoaiGiay, int TrangThai)
+        {
+            var lg = allRepo.GetAll().First(p => p.IdLoaiGiay == idLoaiGiay);
+            lg.TenLoaiGiay = TenLoaiGiay;
+            lg.MaLoaiGiay = MaLoaiGiay;
+            lg.TrangThai = TrangThai;
+            return allRepo.EditItem(lg);
+        }
+
+        // DELETE api/<LoaiGiayController>/5
+        [HttpDelete("Delete")]
+        public bool Delete(Guid idLoaiGiay)
+        {
+            var lg = allRepo.GetAll().First(p => p.IdLoaiGiay == idLoaiGiay);
+            return allRepo.RemoveItem(lg);
+        }
+    }
+}
