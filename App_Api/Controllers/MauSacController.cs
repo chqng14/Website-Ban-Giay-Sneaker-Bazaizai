bbe2343d6ada1a773b4e214dafce94c567ed6037
DESKTOP-GEBT4IE\hoang <boxchatsc1@gmail.com>
2023-09-18 23:20:11 +0700

    add api cho Anh MauSac SanPhamChiTiet XuatXu

---
+++ App_Api/Controllers/MauSacController.cs
@@ -0,0 +1,57 @@
+﻿using App_Data.IRepositories;
+using App_Data.Models;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App_Api.Controllers
+{
+    public class MauSacController : ControllerBase
+    {
+        private readonly IAllRepo<MauSac> _mauSacRespo;
+
+        public MauSacController(IAllRepo<MauSac> mauSacRespo)
+        {
+            _mauSacRespo = mauSacRespo;
+        }
+
+
+        [HttpGet("GetMauSac/{id}")]
+        public MauSac? GetMauSac(Guid id)
+        {
+            return _mauSacRespo.GetAll().FirstOrDefault(x => x.IDMauSac == id);
+        }
+
+
+        [HttpGet("GetAllMauSac")]
+        public List<MauSac> GetListMauSac()
+        {
+            return _mauSacRespo.GetAll().Where(x=>x.TrangThai>0).ToList();
+        }
+
+        [HttpPost("CreateMauSac")]
+        public bool Create(MauSac mauSac)
+        {
+            mauSac.MaMauSac = _mauSacRespo.GetAll().Count() == null ? "MM1" : "MM" + (_mauSacRespo.GetAll().Count() + 1);
+            return _mauSacRespo.AddItem(mauSac);
+        }
+
+        [HttpDelete("DeleteMauSac/{id}")]
+        public bool Delete(MauSac mauSac)
+        {
+            mauSac.TrangThai = 0;
+            return _mauSacRespo.EditItem(mauSac);
+        }
+
+        [HttpDelete("UpdateMauSac/{id}")]
+        public bool Update(MauSac mauSac)
+        {
+            var mauSacGet = GetMauSac(mauSac.IDMauSac);
+            if(mauSacGet != null)
+            {
+                return _mauSacRespo.AddItem(mauSac);
+            }
+            return false;
+        }
+
+    }
+}
---
+++ App_Api/Controllers/XuatXuController.cs
@@ -0,0 +1,56 @@
+﻿using App_Data.IRepositories;
+using App_Data.Models;
+using App_Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App_Api.Controllers
+{
+    public class XuatXuController : ControllerBase
+    {
+        private readonly IAllRepo<XuatXu> _xuatXuRespo;
+
+        public XuatXuController(IAllRepo<XuatXu> xuatXuRespo)
+        {
+            _xuatXuRespo = xuatXuRespo;
+        }
+
+
+        [HttpGet("GetXuatXu/{id}")]
+        public XuatXu? GetXuatXu(Guid id)
+        {
+            return _xuatXuRespo.GetAll().FirstOrDefault(x => x.IDXuatXu == id);
+        }
+
+
+        [HttpGet("GetAllXuatXu")]
+        public List<XuatXu> GetListXuatXu()
+        {
+            return _xuatXuRespo.GetAll().Where(x => x.TrangThai > 0).ToList();
+        }
+
+        [HttpPost("CreateXuatXu")]
+        public bool Create(XuatXu xuatXu)
+        {
+            xuatXu.Ma = _xuatXuRespo.GetAll().Count() == null? "XX1" : "XX" + (_xuatXuRespo.GetAll().Count() + 1);
+            return _xuatXuRespo.AddItem(xuatXu);
+        }
+
+        [HttpDelete("DeleteXuatXu/{id}")]
+        public bool Delete(XuatXu xuatXu)
+        {
+            xuatXu.TrangThai = 0;
+            return _xuatXuRespo.EditItem(xuatXu);
+        }
+
+        [HttpDelete("UpdateXuatXu/{id}")]
+        public bool Update(XuatXu xuatXu)
+        {
+            var xuatXuGet = GetXuatXu(xuatXu.IDXuatXu);
+            if (xuatXuGet != null)
+            {
+                return _xuatXuRespo.AddItem(xuatXu);
+            }
+            return false;
+        }
+    }
+}
