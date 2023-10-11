9f4284d52e667997f5bddb99c6c5bc95fa474a68
BAZAIZAI\ASUS <hieuhxptto@gmail.com>
2023-10-11 22:08:32 +0700

    tạo controller ban hang tai quay

---
+++ App_View/Areas/Admin/Controllers/BanHangTaiQuayController.cs
@@ -0,0 +1,18 @@
+﻿using App_Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App_View.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BanHangTaiQuayController : Controller
+    {
+        public IActionResult DanhSachHoaDonCho()
+        {
+            var fakeData = new List<HoaDon>() {
+                new HoaDon() {MaHoaDon = "HD1"},
+                new HoaDon() {MaHoaDon = "HD2"}
+            };
+            return View();
+        }
+    }
+}
