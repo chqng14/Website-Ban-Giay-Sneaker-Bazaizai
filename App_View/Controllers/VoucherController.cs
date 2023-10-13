10aec324a78bf09aff87301f16eb7944ef5d7720
chqng14 <phuongchang1402@gmail.com>
2023-10-13 23:49:58 +0700

    Xong view cơ bản của nhận voucher mai làm thêm add voucher form Voucher

--- App_View/Controllers/VoucherController.cs
+++ App_View/Controllers/VoucherController.cs
@@ -1,6 +1,7 @@
 ﻿using App_Data.DbContextt;
 using App_Data.Models;
 using App_View.IServices;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using static App_Data.Repositories.TrangThai;
 
@@ -10,14 +11,26 @@ namespace App_View.Controllers
     {
         private readonly BazaizaiContext _context;
         private readonly IVoucherServices _voucherSV;
+        private readonly SignInManager<NguoiDung> _signInManager;
+        private readonly UserManager<NguoiDung> _userManager;
 
-        public VoucherController(IVoucherServices voucherServices)
+        public VoucherController(IVoucherServices voucherServices, SignInManager<NguoiDung> signInManager, UserManager<NguoiDung> userManager)
         {
             _voucherSV = voucherServices;
             _context = new BazaizaiContext();
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
         public async Task<IActionResult> VoucherToCalm()
         {
+            var idNguoiDung = _userManager.GetUserId(User);
+            if (string.IsNullOrEmpty(idNguoiDung))
+            {
+                ViewBag.NguoiDung = null;
+            }
+            else ViewBag.NguoiDung = idNguoiDung;
+
+
             var allVouchers = (await _voucherSV.GetAllVoucher()).Where(c => c.TrangThai == 0);
 
             var tienMatVouchers = allVouchers.Where(c => c.LoaiHinhUuDai == (int)TrangThaiLoaiKhuyenMai.TienMat);
