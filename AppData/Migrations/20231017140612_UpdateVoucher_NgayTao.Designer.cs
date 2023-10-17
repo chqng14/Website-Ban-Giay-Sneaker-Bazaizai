f66fae0a966f9e711a9772103ad81c9031cbcf30
chqng14 <phuongchang1402@gmail.com>
2023-10-17 21:31:36 +0700

    Sửa Database Voucher: THêm thuộc tính Ngày Tạo

--- AppData/Migrations/20231017140612_UpdateVoucher_NgayTao.Designer.cs
+++ AppData/Migrations/20231017140612_UpdateVoucher_NgayTao.Designer.cs
@@ -4,6 +4,7 @@ using App_Data.DbContextt;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace App_Data.Migrations
 {
     [DbContext(typeof(BazaizaiContext))]
-    partial class BazaizaiContextModelSnapshot : ModelSnapshot
+    [Migration("20231017140612_UpdateVoucher_NgayTao")]
+    partial class UpdateVoucher_NgayTao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
