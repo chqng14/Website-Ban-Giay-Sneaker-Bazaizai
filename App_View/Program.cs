13e8772e4ee6efffae1bc0ffab0a361b4688ab9b
chqng14 <phuongchang1402@gmail.com>
2023-11-28 00:30:03 +0700

    Sửa giao diện voucher và thông báo khi nahanj voucher

--- App_View/Program.cs
+++ App_View/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddScoped<IVnPayService, VnPayService>();
 //BAZAIZAI\SQLEXPRESS
 
 
-builder.Services.AddHangfire(x => x.UseSqlServerStorage(@"Data Source=MSI;Initial Catalog=DuAnTotNghiep_BazaizaiStore;Integrated Security=True")); //Đoạn này ai chạy lỗi thì đổi đường dẫn trong này nha
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(@"Data Source=.\SQLEXPRESS;Initial Catalog=DuAnTotNghiep_BazaizaiStore;Integrated Security=True")); //Đoạn này ai chạy lỗi thì đổi đường dẫn trong này nha
 
 
 builder.Services.AddHangfireServer();
