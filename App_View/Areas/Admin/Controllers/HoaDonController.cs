87cb385640d9b7931a11c64c1974aed31129f803
Kaisan003 <khai030720038c@gmail.com>
2023-11-28 22:33:17 +0700

    update status order

--- App_View/Areas/Admin/Controllers/HoaDonController.cs
+++ App_View/Areas/Admin/Controllers/HoaDonController.cs
@@ -1,10 +1,12 @@
 ﻿using App_Data.DbContextt;
 using App_Data.Models;
 using App_Data.ViewModels.HoaDon;
+using App_Data.ViewModels.SanPhamChiTietDTO;
 using App_View.IServices;
 using App_View.Models.ViewModels;
 using App_View.Services;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -13,16 +15,25 @@ namespace App_View.Areas.Admin.Controllers
     [Area("Admin")]
     public class HoaDonController : Controller
     {
-        private readonly IHoaDonServices _hoaDonServices;
+
+		private readonly IVoucherNguoiDungServices _voucherNguoiDungServices;
+		private readonly IVoucherServices _voucherServices;
+		private readonly IHoaDonChiTietServices _hoaDonChiTietServices; private readonly SignInManager<NguoiDung> _signInManager;
+		private readonly UserManager<NguoiDung> _userManager;
+		private readonly IHoaDonServices _hoaDonServices;
         private readonly ISanPhamChiTietService sanPhamChiTietService;
         BazaizaiContext context;
-        public HoaDonController(ISanPhamChiTietService sanPhamChiTietService)
-        {
-            _hoaDonServices = new HoaDonServices();
-            context = new BazaizaiContext();
-            this.sanPhamChiTietService = sanPhamChiTietService;
-        }
-        public IActionResult TongQuanHoaDon()
+		public HoaDonController(ISanPhamChiTietService sanPhamChiTietService, IVoucherNguoiDungServices voucherNguoiDungServices, SignInManager<NguoiDung> signInManager, UserManager<NguoiDung> userManager)
+		{
+			_hoaDonServices = new HoaDonServices();
+			context = new BazaizaiContext();
+			this.sanPhamChiTietService = sanPhamChiTietService;
+			_voucherNguoiDungServices = voucherNguoiDungServices;
+			_hoaDonChiTietServices = new HoaDonChiTietServices();
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
+		public IActionResult TongQuanHoaDon()
         {
             return View();
         }
@@ -77,5 +88,59 @@ namespace App_View.Areas.Admin.Controllers
             ViewBag.HDCT= HDCT;
             return PartialView("_ChiTietHoaDon", hoaDonChiTiet);
         }
-    }
+        [HttpGet]
+        public async Task<IActionResult> InHoaDonTaiQuayAsync(string MaHD)
+        {
+            var hoaDon = (await _hoaDonServices.GetHoaDon()).FirstOrDefault(x=>x.MaHoaDon==MaHD);
+			var hoaDonChiTiet = context.HoaDons.FirstOrDefault(x => x.IdHoaDon == hoaDon.IdHoaDon);
+			ViewBag.TTGH = context.thongTinGiaoHangs.FirstOrDefault(x => x.IdThongTinGH == hoaDon.IdThongTinGH);
+			ViewData["MAHD"] = hoaDon.MaHoaDon;
+			ViewData["NGAYTAO"] = hoaDon.NgayTao;
+			ViewData["TIENSHIP"] = hoaDon.TienShip;
+			ViewData["TONGTIEN"] = hoaDon.TongTien;
+			ViewData["TIENGIAM"] = hoaDon.TienGiam;
+			var HDCT = context.hoaDonChiTiets.Where(x => x.IdHoaDon == hoaDon.IdHoaDon);
+            ViewBag.HDCT = HDCT;
+            return PartialView("InHoaDonTaiQuay", hoaDonChiTiet);
+        }
+        [HttpPost]
+		public async Task<IActionResult> HuyHoaDon(string maHD, string lyDoHuy)
+		{
+			if (string.IsNullOrEmpty(maHD))
+			{
+				return Ok(new
+				{
+					TrangThai = false,
+				});
+			}
+			var hoaDon = (await _hoaDonServices.GetAllHoaDonCho()).FirstOrDefault(c => c.MaHoaDon == maHD);
+			var user = await _userManager.GetUserAsync(User);
+			var idUser = await _userManager.GetUserIdAsync(user);
+			var hoadonchitiet = await _hoaDonChiTietServices.HuyHoaDon(maHD, lyDoHuy, idUser);
+			if (hoaDon.hoaDonChiTietDTOs.Count() == 0)
+			{
+				return Ok(
+				  new { TrangThai = true, }
+				  );
+			}
+			if (hoadonchitiet.Any())
+			{
+				foreach (var item in hoadonchitiet)
+				{
+					await sanPhamChiTietService.UpDatSoLuongAynsc(new SanPhamSoLuongDTO()
+					{
+						IdChiTietSanPham = item.IdSanPhamChiTiet,
+						SoLuong = -(int)item.SoLuong,
+					});
+				}
+				return Ok(
+					new { TrangThai = true, }
+					);
+			}
+			return Ok(new
+			{
+				TrangThai = false,
+			});
+		}
+	}
 }
