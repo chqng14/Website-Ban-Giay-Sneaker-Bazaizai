3ae0c5f4100d91c44ac1520024e04ab75e94aee6
Nguyễn Quang Khải (dev) <khainq@ssoftware.vn>
2024-03-22 09:28:33 +0700

    bảo trì dự án

--- App_View/Areas/Admin/Controllers/KhuyenMaisController.cs
+++ App_View/Areas/Admin/Controllers/KhuyenMaisController.cs
@@ -21,6 +21,9 @@ using App_Data.Repositories;
 using Google.Apis.PeopleService.v1.Data;
 using Org.BouncyCastle.Pqc.Crypto.Lms;
 using Microsoft.AspNetCore.Authorization;
+using MimeKit;
+using System.Net.Http.Headers;
+using DocumentFormat.OpenXml.Office2010.Excel;
 
 namespace App_View.Areas.Admin.Controllers
 {
@@ -167,20 +170,21 @@ namespace App_View.Areas.Admin.Controllers
                 {
 
                     using var content = new MultipartFormDataContent();
-                    //content.Add(new StringContent(khuyenMai.TrangThai.ToString()), "trangThai");
-                    //content.Add(new StringContent(khuyenMai.TenKhuyenMai), "Ten");
-                    //content.Add(new StringContent(khuyenMai.NgayBatDau.ToString()), "ngayBD");
-                    //content.Add(new StringContent(khuyenMai.NgayKetThuc.ToString()), "ngayKT");
-                    //content.Add(new StringContent(khuyenMai.LoaiHinhKM.ToString()), "loaiHinh");
-                    //content.Add(new StringContent(khuyenMai.PhamVi), "PhamVi");
-                    //content.Add(new StringContent(khuyenMai.MucGiam.ToString()), "mucGiam");
+                    content.Add(new StringContent($"{khuyenMai.LoaiHinhKM}"), "LoaiHinhKM");
+                    content.Add(new StringContent($"{khuyenMai.MucGiam}"), "MucGiam");
+                    content.Add(new StringContent($"{khuyenMai.NgayKetThuc?.ToString("dd/MM/yyyy hh:mm:ss tt")}"), "NgayKetThuc");
+                    content.Add(new StringContent($"{khuyenMai.TenKhuyenMai}"), "TenKhuyenMai");
+                    content.Add(new StringContent($"{khuyenMai.IdKhuyenMai}"), "IdKhuyenMai");
+                    content.Add(new StringContent($"{khuyenMai.NgayBatDau?.ToString("dd/MM/yyyy hh:mm:ss tt")}"), "NgayBatDau");
+                    content.Add(new StringContent("111"), "MaKhuyenMai");
+                    content.Add(new StringContent($"{khuyenMai.TrangThai}"), "TrangThai");
                     if (formFile != null && formFile.Length > 0)
                     {
 
                         var streamContent = new StreamContent(formFile.OpenReadStream());
                         streamContent.Headers.Add("Content-Type", formFile.ContentType);
                         content.Add(streamContent, "formFile", formFile.FileName);
-                        var response = await _httpClient.PostAsync($"https://localhost:7038/api/KhuyenMai/Create-KhuyenMai?id={khuyenMai.IdKhuyenMai}&Ten={khuyenMai.TenKhuyenMai}&ngayBD={khuyenMai.NgayBatDau}&ngayKT={khuyenMai.NgayKetThuc}&trangThai={khuyenMai.TrangThai}&mucGiam={khuyenMai.MucGiam}&loaiHinh={khuyenMai.LoaiHinhKM}", content);
+                        var response = await _httpClient.PostAsync($"https://localhost:7038/api/KhuyenMai/Create-KhuyenMai", content);
                         if (response.IsSuccessStatusCode)
                         {
                             var lstUser = await _userManager.GetUsersInRoleAsync(ChucVuMacDinh.KhachHang.ToString());
@@ -314,45 +318,48 @@ namespace App_View.Areas.Admin.Controllers
                 {
 
                     using var content = new MultipartFormDataContent();
-                    //content.Add(new StringContent(khuyenMai.TrangThai.ToString()), "trangThai");
-                    //content.Add(new StringContent(khuyenMai.TenKhuyenMai), "Ten");
-                    //content.Add(new StringContent(khuyenMai.NgayBatDau.ToString()), "ngayBD");
-                    //content.Add(new StringContent(khuyenMai.NgayKetThuc.ToString()), "ngayKT");
-                    //content.Add(new StringContent(khuyenMai.LoaiHinhKM.ToString()), "loaiHinh");
-                    //content.Add(new StringContent(khuyenMai.PhamVi), "PhamVi");
-                    //content.Add(new StringContent(khuyenMai.MucGiam.ToString()), "mucGiam");
+                    content.Add(new StringContent($"{khuyenMai.LoaiHinhKM}"), "LoaiHinhKM");
+                    content.Add(new StringContent($"{khuyenMai.MucGiam}"), "MucGiam");
+                    content.Add(new StringContent($"{khuyenMai.NgayKetThuc?.ToString("dd/MM/yyyy hh:mm:ss tt")}"), "NgayKetThuc");
+                    content.Add(new StringContent($"{khuyenMai.TenKhuyenMai}"), "TenKhuyenMai");
+                    content.Add(new StringContent($"{id}"), "IdKhuyenMai");
+                    content.Add(new StringContent($"{khuyenMai.NgayBatDau?.ToString("dd/MM/yyyy hh:mm:ss tt")}"), "NgayBatDau");
+                    content.Add(new StringContent("111"), "MaKhuyenMai");
+                    content.Add(new StringContent($"{khuyenMai.TrangThai}"), "TrangThai");
                     if (formFile != null && formFile.Length > 0)
-                    {
-
-                        var streamContent = new StreamContent(formFile.OpenReadStream());
-                        streamContent.Headers.Add("Content-Type", formFile.ContentType);
-                        content.Add(streamContent, "formFile", formFile.FileName);
-                        var response = await _httpClient.PutAsync($"https://localhost:7038/api/KhuyenMai/{id}?Ten={khuyenMai.TenKhuyenMai}&ngayBD={khuyenMai.NgayBatDau}&ngayKT={khuyenMai.NgayKetThuc}&trangThai={khuyenMai.TrangThai}&mucGiam={khuyenMai.MucGiam}&loaiHinh={khuyenMai.LoaiHinhKM}", content);
-                        if (response.IsSuccessStatusCode)
                         {
 
-                            return RedirectToAction("Index");
+                            var streamContent = new StreamContent(formFile.OpenReadStream());
+                            streamContent.Headers.Add("Content-Type", formFile.ContentType);
+                            content.Add(streamContent, "formFile", formFile.FileName);
+                            string urlApi = $"https://localhost:7038/api/KhuyenMai/Edit";
+                            var response = await _httpClient.PutAsync(urlApi, content);
+                                if (response.IsSuccessStatusCode)
+                                {
+
+                                    return RedirectToAction("Index");
+                                }
+                                else
+                                {
+                                    Console.WriteLine(response.StatusCode);
+                                    return BadRequest();
+                                }
+                            }
+                            else {
+                            khuyenMai.IdKhuyenMai = id;
+                            var a = khuyenMai.NgayBatDau?.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+                            var response = await _httpClient.PutAsJsonAsync($"https://localhost:7038/api/KhuyenMai/EditNoiImage", khuyenMai);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return RedirectToAction("Index");
+                            }
+                            else
+                            {
+                                Console.WriteLine(response.StatusCode);
+                                return BadRequest();
+                            }
                         }
-                        else
-                        {
-                            Console.WriteLine(response.StatusCode);
-                            return BadRequest();
-                        }
                     }
-                    else {
-                    khuyenMai.IdKhuyenMai = id;
-                    var response = await _httpClient.PutAsync($"https://localhost:7038/api/KhuyenMai/EditNoiImage?id={id}&Ten={khuyenMai.TenKhuyenMai}&ngayBD={khuyenMai.NgayBatDau}&ngayKT={khuyenMai.NgayKetThuc}&trangThai={khuyenMai.TrangThai}&mucGiam={khuyenMai.MucGiam}&loaiHinh={khuyenMai.LoaiHinhKM}", null);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        return RedirectToAction("Index");
-                    }
-                    else
-                    {
-                        Console.WriteLine(response.StatusCode);
-                        return BadRequest();
-                    }
-                }
-                }
             return View(khuyenMai);
         }
 
